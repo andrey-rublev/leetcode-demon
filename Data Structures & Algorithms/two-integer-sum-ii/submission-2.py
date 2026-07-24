dfd88c16b876d3253ab0220a0b17abb4a010cc50
Nikhil Kolli <95324143+andrey-rublev@users.noreply.github.com>
2026-07-24 09:18:08 -0500

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        freq = Counter(numbers)
+        for i in range(len(numbers)):
+            opp = target - numbers[i]
+            if freq[opp]>0 and numbers[i]!=opp:
+                return [i+1, numbers.index(opp)+1]
+            
